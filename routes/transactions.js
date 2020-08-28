311d4c8bfe766c3443fa2ce1fff2c0d733200964
Jordan McKenzie <jordanmckenz@gmail.com>
2020-08-28 16:54:23 -0700

    added mongodb stuff

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {getTransactions, addTransaction, deleteTransaction } = require('../controllers/transactionControl');
-
-
+const { getTransactions, addTransaction, deleteTransaction } = require('../controllers/transactionControl');
 
 router.route('/').get(getTransactions).post(addTransaction);
 
